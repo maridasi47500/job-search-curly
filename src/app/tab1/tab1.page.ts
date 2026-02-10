c049aff2eb2094504de4a665c88cd423b8a579eb
anonymous Del Barco <61146153+maridasi47500@users.noreply.github.com>
2026-02-10 19:09:29 -0300

    my job search

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { IonActionSheet, IonButton, IonAlert } from '@ionic/angular/standalone';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonPopover, IonMenu, IonHeader, IonButtons, IonMenuButton, IonTitle, IonToolbar, InfiniteScrollCustomEvent, IonModal, IonInfiniteScroll, IonInfiniteScrollContent, IonChip, IonActionSheet, IonButton, IonAlert, IonBadge, IonLabel, IonList, IonItem, IonBreadcrumb, IonBreadcrumbs } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonActionSheet, IonButton, IonAlert],
+  imports: [IonMenu, IonPopover, IonHeader, IonButtons, IonMenuButton, IonTitle, IonToolbar, IonContent, IonChip, IonActionSheet, IonModal, IonButton, IonAlert, IonBadge, IonItem, IonLabel, IonList, IonBreadcrumb, IonBreadcrumbs, IonInfiniteScrollContent, IonInfiniteScroll],
   standalone: false,
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  items: string[] = [];
+  isModalOpen = false;
+  @ViewChild('popover') popover!: HTMLIonPopoverElement;
+
+  isOpen = false;
+
+  presentPopover(e: Event) {
+    this.popover.event = e;
+    this.isOpen = true;
+  }
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
   alertButtons = ['Close'];
   public actionSheetButtons = [
     {
@@ -18,12 +31,18 @@ export class Tab1Page {
         action: 'delete',
       },
 
-    }
+    },
     {
-      text: 'Share',
-      role: 'destructive',
+      text: 'Save',
       data: {
-        action: 'share',
+        action: 'save',
+      },
+
+    },
+    {
+      text: 'Apply',
+      data: {
+        action: 'apply',
       },
 
     },
@@ -36,6 +55,23 @@ export class Tab1Page {
 
     },
   ];
+  ngOnInit() {
+    this.generateItems();
+  }
+  private generateItems(){
+    const count=this.items.length + 1;
+    for (let i = 0; i < 50; i++){
+      this.items.push(`Items ${count + i}`);
+    }
+  }
+  onIonInfinite(event: InfiniteScrollCustomEvent) {
+    this.generateItems();
+    setTimeout(() => {
+      event.target.complete();
+    }, 500);
+  }
+ 
+  
 
   constructor() {}
 
