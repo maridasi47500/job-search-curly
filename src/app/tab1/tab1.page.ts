fe890da60d2f25884e7796e871da1a1f3809320a
anonymous Del Barco <61146153+maridasi47500@users.noreply.github.com>
2026-02-10 17:52:58 -0300

    hey

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { IonActionSheet, IonButton, IonAlert } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
+  imports: [IonActionSheet, IonButton, IonAlert],
   standalone: false,
 })
 export class Tab1Page {
+  alertButtons = ['Close'];
+  public actionSheetButtons = [
+    {
+      text: 'Delete',
+      role: 'destructive',
+      data: {
+        action: 'delete',
+      },
+
+    }
+    {
+      text: 'Share',
+      role: 'destructive',
+      data: {
+        action: 'share',
+      },
+
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+
+    },
+  ];
 
   constructor() {}
 
